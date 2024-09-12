2014e37d5b898f3c0cb36af8be9bf45d53186ca0
JPham4801 <JPham4801@gmail.com>
2024-09-12 12:46:32 -0500

    deployment

--- myPOV/settings.py
+++ myPOV/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if not "ON_HEROKU" in os.environ:
-    DEBUG = False
+    DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
